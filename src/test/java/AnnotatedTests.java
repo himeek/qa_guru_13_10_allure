85968fd707922a700e43155b9bdf5a262c4c329c
himeek <himeek11@gmail.com>
2022-06-30 21:41:17 +0300

    first commit

---
+++ src/test/java/AnnotatedTests.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+    private final String REPOSITORY = "eroshenkoam/allure-example";
+    private final String ISSUE_LABEL = "69 nice";
+    @Test
+    public void testAnnotatedSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebStepsTests steps = new WebStepsTests();
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.openRepositoryLink(REPOSITORY);
+        steps.openIssueTab();
+        steps.shouldSeeIssueWithNumber(ISSUE_LABEL);
+    }
+}
